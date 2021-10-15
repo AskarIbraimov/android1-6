19785ec401acad133ef8e4c29ed8dc4f790a0f0b
AskarIbraimov <raksakgz@gmail.com>
2021-10-15 16:12:57 +0600

    coins

--- app/src/main/java/com/example/android1_6/data/local/Data.java
+++ app/src/main/java/com/example/android1_6/data/local/Data.java
@@ -11,12 +11,12 @@ public class Data {
 
     public ArrayList<Coin> getList() {
         list = new ArrayList<>();
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
-        list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
+        list.add(new Coin("V-coin","1.00", R.drawable.vcoin ));
+        list.add(new Coin("B-coin +","4.00", R.drawable.bcoin ));
+        list.add(new Coin("Label","0.00", R.drawable.label ));
+        list.add(new Coin("S-coin","6.00", R.drawable.scoin ));
+        list.add(new Coin("T-coin","0.00", R.drawable.tcoin ));
+        list.add(new Coin("X-coin","40.00", R.drawable.xcoin ));
         list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
         list.add(new Coin("V coin","1.00", R.drawable.vcoin ));
 
